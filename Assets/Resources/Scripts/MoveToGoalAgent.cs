355f90826cd697d81863f7df0069107efc5efc00
James Churcher <jc01901@surrey.ac.uk>
2022-04-17 03:21:02 +0100

    Use Sin/Cos for rotation features, sigmoid normalises distance vector

--- Assets/Resources/Scripts/MoveToGoalAgent.cs
+++ Assets/Resources/Scripts/MoveToGoalAgent.cs
@@ -43,16 +43,30 @@ public class MoveToGoalAgent : Agent
             AddReward(+1f);
         }
     }
+
+    private static float Sigmoid(float value)
+    {
+        return 1.0f / (1.0f + (float)Mathf.Exp(-value));
+    }
+
     public override void CollectObservations(VectorSensor sensor)
     {
+        // Get distance vector between player and target
         Vector2 distance = targetObject.transform.position - playerObject.transform.position;
+        distance /= 15;
+        distance.x = (Sigmoid(distance.x) * 2) - 1;
+        distance.y = (Sigmoid(distance.y) * 2) - 1;
 
-        float rotation = playerObject.transform.rotation.eulerAngles.z;
-        rotation -= 180;
+        // Get rotation of player, convert to sin and cos
+        // If angle needed back use arctan2(sin, cos)
+        float r = playerObject.transform.rotation.eulerAngles.z * Mathf.Deg2Rad;
+        Vector2 rotation = new(Mathf.Sin(r), Mathf.Cos(r));
 
-        //List<RaycastHit2D> raycasts = playerObject.GetComponent<BotAPI>().getRayCasts();
+        // Get raycast distances
         List<RaycastHit2D> raycasts = botAPI.getRayCasts();
-        Vector2 rays = new(raycasts[0].distance, raycasts[1].distance);
+        Vector2 rays = Vector2.zero;
+        rays[0] = raycasts[0].distance / botAPI.rayCaster.distance;
+        rays[1] = raycasts[1].distance / botAPI.rayCaster.distance;
 
         sensor.AddObservation(distance);
         sensor.AddObservation(rotation);
@@ -64,6 +78,8 @@ public class MoveToGoalAgent : Agent
         float forwardVel = actions.ContinuousActions[0];
         float adjacentVel = actions.ContinuousActions[1];
         float angularVel = actions.ContinuousActions[2];
+
+        print(("Fwd, Adj, Ang: ", forwardVel, adjacentVel, angularVel));
 
         botAPI.setForwardVel(forwardVel);
         botAPI.setAdjacentVel(adjacentVel);
